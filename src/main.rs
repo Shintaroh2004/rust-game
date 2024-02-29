781af3fd2c06924d84f42bd889ada1921506815e
Shintaro2004 <watanabe20040802@gmail.com>
2024-02-29 13:38:24 +0900

    wasd操作とカメラ制御を可能に．

---
+++ src/main.rs
@@ -0,0 +1,412 @@
+#![cfg_attr(target_arch = "wasm32", allow(clippy::arc_with_non_send_sync))]
+
+use std::{f32::consts::PI, sync::Arc};
+use winit::{dpi::PhysicalPosition, event::ElementState};
+use glam::Vec4Swizzles;
+
+struct State {
+    forward:bool,
+    back:bool,
+    left:bool,
+    right:bool,
+    pre_cursor_pos:PhysicalPosition<f32>,
+    euler:glam::Mat4,
+    euler_x:f32,
+    euler_y:f32,
+}
+
+impl State{
+    fn new()->Self{
+        Self{
+            forward:false,
+            back:false,
+            left:false,
+            right:false,
+            pre_cursor_pos:PhysicalPosition{x:955.0,y:515.0},
+            euler:glam::Mat4::from_euler(glam::EulerRot::XYZ, 0.0, 0.0, 0.0),
+            euler_x:0.0,
+            euler_y:0.0,
+        }
+    }
+
+    fn euler_deg_update(&mut self,position:&PhysicalPosition<f64>){
+        let deff_euler_deg=glam::Vec2{
+            y:((position.x as f32)-self.pre_cursor_pos.x)/300.0,
+            x:((position.y as f32)-self.pre_cursor_pos.y)/300.0
+        };
+        let euler_x=self.euler_x-deff_euler_deg.x;
+        if (euler_x<=(PI/2.5)) && (euler_x>=(-PI/2.5)){
+            self.euler_x-=deff_euler_deg.x;
+        }else if euler_x > (PI/2.5){
+            self.euler_x=PI/2.5;
+        }else if euler_x < -(PI/2.5){
+            self.euler_x=-(PI/2.5);
+        }
+        self.euler_y-=deff_euler_deg.y;
+        self.euler=glam::Mat4::from_euler(glam::EulerRot::XYZ, self.euler_x, self.euler_y, 0.0);
+    }
+}
+
+fn vertex(pos: [f32; 3]) -> glam::Vec3 {
+    glam::Vec3::from(pos)
+}
+
+fn create_mesh() -> rend3::types::Mesh {
+    let vertex_positions = [
+        // far side (0.0, 0.0, 1.0)
+        vertex([-1.0, -1.0, 1.0]),
+        vertex([1.0, -1.0, 1.0]),
+        vertex([1.0, 1.0, 1.0]),
+        vertex([-1.0, 1.0, 1.0]),
+        // near side (0.0, 0.0, -1.0)
+        vertex([-1.0, 1.0, -1.0]),
+        vertex([1.0, 1.0, -1.0]),
+        vertex([1.0, -1.0, -1.0]),
+        vertex([-1.0, -1.0, -1.0]),
+        // right side (1.0, 0.0, 0.0)
+        vertex([1.0, -1.0, -1.0]),
+        vertex([1.0, 1.0, -1.0]),
+        vertex([1.0, 1.0, 1.0]),
+        vertex([1.0, -1.0, 1.0]),
+        // left side (-1.0, 0.0, 0.0)
+        vertex([-1.0, -1.0, 1.0]),
+        vertex([-1.0, 1.0, 1.0]),
+        vertex([-1.0, 1.0, -1.0]),
+        vertex([-1.0, -1.0, -1.0]),
+        // top (0.0, 1.0, 0.0)
+        vertex([1.0, 1.0, -1.0]),
+        vertex([-1.0, 1.0, -1.0]),
+        vertex([-1.0, 1.0, 1.0]),
+        vertex([1.0, 1.0, 1.0]),
+        // bottom (0.0, -1.0, 0.0)
+        vertex([1.0, -1.0, 1.0]),
+        vertex([-1.0, -1.0, 1.0]),
+        vertex([-1.0, -1.0, -1.0]),
+        vertex([1.0, -1.0, -1.0]),
+    ];
+
+    let index_data: &[u32] = &[
+        0, 1, 2, 2, 3, 0, // far
+        4, 5, 6, 6, 7, 4, // near
+        8, 9, 10, 10, 11, 8, // right
+        12, 13, 14, 14, 15, 12, // left
+        16, 17, 18, 18, 19, 16, // top
+        20, 21, 22, 22, 23, 20, // bottom
+    ];
+
+    rend3::types::MeshBuilder::new(vertex_positions.to_vec(), rend3::types::Handedness::Left)
+        .with_indices(index_data.to_vec())
+        .build()
+        .unwrap()
+}
+
+fn main() {
+
+    let mut my_state=State::new();
+
+    // Setup logging
+    env_logger::init();
+
+    // Create event loop and window
+    let event_loop = winit::event_loop::EventLoop::new().unwrap();
+    let window = {
+        let mut builder = winit::window::WindowBuilder::new();
+        builder = builder.with_title("rend3 cube");
+        builder.build(&event_loop).expect("Could not build window")
+    };
+    let window=Arc::new(window);
+
+    let window_size = window.inner_size();
+
+    // Create the Instance, Adapter, and Device. We can specify preferred backend,
+    // device name, or rendering profile. In this case we let rend3 choose for us.
+    let iad = pollster::block_on(rend3::create_iad(None, None, None, None)).unwrap();
+
+    // The one line of unsafe needed. We just need to guarentee that the window
+    // outlives the use of the surface.
+    //
+    // SAFETY: this surface _must_ not be used after the `window` dies. Both the
+    // event loop and the renderer are owned by the `run` closure passed to winit,
+    // so rendering work will stop after the window dies.
+    let surface = Arc::new(iad.instance.create_surface(window.clone()).unwrap());
+    // Get the preferred format for the surface.
+    let caps = surface.get_capabilities(&iad.adapter);
+    let preferred_format = caps.formats[0];
+
+    // Configure the surface to be ready for rendering.
+    rend3::configure_surface(
+        &surface,
+        &iad.device,
+        preferred_format,
+        glam::UVec2::new(window_size.width, window_size.height),
+        rend3::types::PresentMode::Fifo,
+    );
+
+    // Make us a renderer.
+    let renderer = rend3::Renderer::new(
+        iad,
+        rend3::types::Handedness::Left,
+        Some(window_size.width as f32 / window_size.height as f32),
+    )
+    .unwrap();
+
+    // Create the shader preprocessor with all the default shaders added.
+    let mut spp = rend3::ShaderPreProcessor::new();
+    rend3_routine::builtin_shaders(&mut spp);
+
+    // Create the base rendergraph.
+    let base_rendergraph = rend3_routine::base::BaseRenderGraph::new(&renderer, &spp);
+
+    let mut data_core = renderer.data_core.lock();
+    let pbr_routine = rend3_routine::pbr::PbrRoutine::new(
+        &renderer,
+        &mut data_core,
+        &spp,
+        &base_rendergraph.interfaces,
+        &base_rendergraph.gpu_culler.culling_buffer_map_handle,
+    );
+    drop(data_core);
+    let tonemapping_routine = rend3_routine::tonemapping::TonemappingRoutine::new(
+        &renderer,
+        &spp,
+        &base_rendergraph.interfaces,
+        preferred_format,
+    );
+
+    // Create mesh and calculate smooth normals based on vertices
+    let mesh = create_mesh();
+
+    // Add mesh to renderer's world.
+    //
+    // All handles are refcounted, so we only need to hang onto the handle until we
+    // make an object.
+    let mesh_handle = renderer.add_mesh(mesh).unwrap();
+
+    // Add PBR material with all defaults except a single color.
+    let material = rend3_routine::pbr::PbrMaterial {
+        albedo: rend3_routine::pbr::AlbedoComponent::Value(glam::Vec4::new(0.3, 0.6, 0.9, 1.0)),
+        ..rend3_routine::pbr::PbrMaterial::default()
+    };
+    let material_handle = renderer.add_material(material);
+
+    // Combine the mesh and the material with a location to give an object.
+    let object = rend3::types::Object {
+        mesh_kind: rend3::types::ObjectMeshKind::Static(mesh_handle),
+        material: material_handle,
+        transform: glam::Mat4::IDENTITY,
+    };
+
+    // Creating an object will hold onto both the mesh and the material
+    // even if they are deleted.
+    //
+    // We need to keep the object handle alive.
+
+    let _object_handle = renderer.add_object(object);
+
+    let view_location = glam::Vec3::new(0.0, 0.0, -5.0);
+    let view = glam::Mat4::from_euler(glam::EulerRot::XYZ, 0.0, 0.0, 0.0);
+    let view = view * glam::Mat4::from_translation(-view_location);
+
+    // Set camera's location
+    renderer.set_camera_data(rend3::types::Camera {
+        projection: rend3::types::CameraProjection::Perspective { vfov: 60.0, near: 0.1 },
+        view,
+    });
+
+    // Create a single directional light
+    //
+    // We need to keep the directional light handle alive.
+    let _directional_handle = renderer.add_directional_light(rend3::types::DirectionalLight {
+        color: glam::Vec3::ONE,
+        intensity: 10.0,
+        // Direction will be normalized
+        direction: glam::Vec3::new(-1.0, -4.0, 2.0),
+        distance: 400.0,
+        resolution: 2048,
+    });
+
+    let mut resolution = glam::UVec2::new(window_size.width, window_size.height);
+
+    let mut transration=glam::vec3(0.0, 0.5, -5.0);
+
+    event_loop
+        .run(move |event, _event_loop_window_target| match event {
+            // Window was resized, need to resize renderer.
+            winit::event::Event::WindowEvent {
+                event: winit::event::WindowEvent::Resized(physical_size),
+                ..
+            } => {
+                resolution = glam::UVec2::new(physical_size.width, physical_size.height);
+                // Reconfigure the surface for the new size.
+                rend3::configure_surface(
+                    &surface,
+                    &renderer.device,
+                    preferred_format,
+                    glam::UVec2::new(resolution.x, resolution.y),
+                    rend3::types::PresentMode::Fifo,
+                );
+                // Tell the renderer about the new aspect ratio.
+                renderer.set_aspect_ratio(resolution.x as f32 / resolution.y as f32);
+            }
+            // Render!
+            winit::event::Event::WindowEvent {
+                event: winit::event::WindowEvent::RedrawRequested,
+                ..
+            } => {
+                // Get a frame
+                let frame = surface.get_current_texture().unwrap();
+
+                // Swap the instruction buffers so that our frame's changes can be processed.
+                renderer.swap_instruction_buffers();
+
+                // Evaluate our frame's world-change instructions
+                let mut eval_output = renderer.evaluate_instructions();
+
+                // Build a rendergraph
+                let mut graph = rend3::graph::RenderGraph::new();
+
+                // Import the surface texture into the render graph.
+                let frame_handle = graph.add_imported_render_target(
+                    &frame,
+                    0..1,
+                    0..1,
+                    rend3::graph::ViewportRect::from_size(resolution),
+                );
+                // Add the default rendergraph without a skybox
+                base_rendergraph.add_to_graph(
+                    &mut graph,
+                    rend3_routine::base::BaseRenderGraphInputs {
+                        eval_output: &eval_output,
+                        routines: rend3_routine::base::BaseRenderGraphRoutines {
+                            pbr: &pbr_routine,
+                            skybox: None,
+                            tonemapping: &tonemapping_routine,
+                        },
+                        target: rend3_routine::base::OutputRenderTarget {
+                            handle: frame_handle,
+                            resolution,
+                            samples: rend3::types::SampleCount::One,
+                        },
+                    },
+                    rend3_routine::base::BaseRenderGraphSettings {
+                        ambient_color: glam::Vec4::ZERO,
+                        clear_color: glam::Vec4::new(0.10, 0.15, 0.30, 1.0), // Nice scene-referred purple
+                    },
+                );
+
+                // Dispatch a render using the built up rendergraph!
+                graph.execute(&renderer, &mut eval_output);
+
+                // Present the frame
+                frame.present();
+
+                if my_state.forward{
+                    let vel_mat=my_state.euler*glam::Mat4::from_translation(glam::vec3(0.0,0.0,1.0));
+                    let vel=vel_mat.w_axis.xyz();
+                    let vel=vel.normalize()*0.2;
+                    transration.x-=vel.x;
+                    transration.z+=vel.z;
+                }
+                if my_state.back{
+                    let vel_mat=my_state.euler*glam::Mat4::from_translation(glam::vec3(0.0,0.0,1.0));
+                    let vel=vel_mat.w_axis.xyz();
+                    let vel=vel.normalize()*0.2;
+                    transration.x+=vel.x;
+                    transration.z-=vel.z;
+                }
+                if my_state.left{
+                    let vel_mat=glam::Mat4::from_rotation_y(PI/2.0)*my_state.euler*glam::Mat4::from_translation(glam::vec3(0.0,0.0,1.0));
+                    let vel=vel_mat.w_axis.xyz();
+                    let vel=vel.normalize()*0.2;
+                    transration.x-=vel.x;
+                    transration.z+=vel.z;
+                }
+                if my_state.right{
+                    let vel_mat=glam::Mat4::from_rotation_y(-PI/2.0)*my_state.euler*glam::Mat4::from_translation(glam::vec3(0.0,0.0,1.0));
+                    let vel=vel_mat.w_axis.xyz();
+                    let vel=vel.normalize()*0.2;
+                    transration.x-=vel.x;
+                    transration.z+=vel.z;
+                }
+
+                let view_location = transration;
+                let view = my_state.euler;
+                let view = view * glam::Mat4::from_translation(-view_location);
+
+                renderer.set_camera_data(rend3::types::Camera {
+                    projection: rend3::types::CameraProjection::Perspective { vfov: 60.0, near: 0.1 },
+                    view,
+                });
+
+                window.request_redraw();
+            }
+            //KeyBordEvents
+            winit::event::Event::WindowEvent {
+                event:winit::event::WindowEvent::KeyboardInput{
+                    event:winit::event::KeyEvent{
+                        physical_key,
+                        state,
+                        repeat,
+                        ..
+                    },
+                    ..
+                },
+                ..
+            }=>{
+                match state {
+                    ElementState::Pressed=>{
+                        if !repeat {
+                            match physical_key {
+                                winit::keyboard::PhysicalKey::Code(winit::keyboard::KeyCode::KeyW)=>{
+                                    my_state.forward=true;
+                                },
+                                winit::keyboard::PhysicalKey::Code(winit::keyboard::KeyCode::KeyA)=>{
+                                    my_state.left=true;
+                                },
+                                winit::keyboard::PhysicalKey::Code(winit::keyboard::KeyCode::KeyS)=>{
+                                    my_state.back=true;
+                                },
+                                winit::keyboard::PhysicalKey::Code(winit::keyboard::KeyCode::KeyD)=>{
+                                    my_state.right=true;
+                                },
+                                _=>{}
+                            }
+                        }    
+                    },
+                    ElementState::Released=>{
+                        match physical_key {
+                            winit::keyboard::PhysicalKey::Code(winit::keyboard::KeyCode::KeyW)=>{
+                                my_state.forward=false;
+                            },
+                            winit::keyboard::PhysicalKey::Code(winit::keyboard::KeyCode::KeyA)=>{
+                                my_state.left=false;
+                            },
+                            winit::keyboard::PhysicalKey::Code(winit::keyboard::KeyCode::KeyS)=>{
+                                my_state.back=false;
+                            },
+                            winit::keyboard::PhysicalKey::Code(winit::keyboard::KeyCode::KeyD)=>{
+                                my_state.right=false;
+                            },
+                            _=>{}
+                        }
+                    },
+                }
+            }
+            //MouseCursor
+            winit::event::Event::WindowEvent { 
+                event:winit::event::WindowEvent::CursorMoved { 
+                    position,
+                    ..
+                },
+                ..
+            }=>{
+                my_state.euler_deg_update(&position);
+
+                window.set_cursor_visible(false);
+                if let Ok(_)=window.set_cursor_position(my_state.pre_cursor_pos){}
+            }
+            // Other events we don't care about
+            _ => {}
+        })
+        .expect("");
+}
